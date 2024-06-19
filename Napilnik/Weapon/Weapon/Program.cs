61479e9b0ffd3ff4edf678f940aea437dd8f141e
BatDen52 <kaluzhin.denis@gmail.com>
2024-06-19 18:41:44 +0300

    Weapon HW

---
+++ Napilnik/Weapon/Weapon/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace HW1.Weapon
+{
+    class Weapon
+    {
+        public int Damage { get; private set; }
+        public int Bullets { get; private set; }
+        public int BulletsPerShot { get; private set; }
+
+        public void Fire(Player player)
+        {
+            Bullets -= BulletsPerShot;
+            player.TakeDamage(Damage);
+        }
+    }
+
+    class Player
+    {
+        public Health Health { get; private set; }
+
+        public void TakeDamage(int damage) => Health.ApplyDamage(damage);
+    }
+
+    class Health
+    {
+        public int MinValue { get; private set; } = 0;
+        public int MaxHealth { get; private set; } = 100;
+        public int Value { get; private set; }
+
+        public void ApplyDamage(int damage)
+        {
+            if (damage < 0)
+                throw new ArgumentException();
+
+            if (damage <= Value)
+                Value -= damage;
+            else
+                Value = MinValue;
+        }
+    }
+
+    class Bot
+    {
+        public Weapon Weapon { get; private set; }
+
+        public void OnSeePlayer(Player player) => Weapon.Fire(player);
+    } 
+}
